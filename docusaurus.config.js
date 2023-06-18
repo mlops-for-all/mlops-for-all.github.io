e444c371fbbae849f77bc2849e1d2c14a4bb6ff8
Aiden-Jeon <aiden.jongseob@gmail.com>
2023-06-19 01:17:08 +0900

    add makinarocks logo
    Signed-off-by: Aiden-Jeon <aiden.jongseob@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -132,48 +132,12 @@ const config = {
       },
       footer: {
         style: "dark",
-        // links: [
-        //   {
-        //     title: 'Docs',
-        //     items: [
-        //       {
-        //         label: 'Tutorial',
-        //         to: '/docs/intro',
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     title: 'Community',
-        //     items: [
-        //       {
-        //         label: 'Stack Overflow',
-        //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-        //       },
-        //       {
-        //         label: 'Discord',
-        //         href: 'https://discordapp.com/invite/docusaurus',
-        //       },
-        //       {
-        //         label: 'Twitter',
-        //         href: 'https://twitter.com/docusaurus',
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     title: 'More',
-        //     items: [
-        //       {
-        //         label: 'Blog',
-        //         to: '/blog',
-        //       },
-        //       {
-        //         label: 'GitHub',
-        //         href: 'https://github.com/facebook/docusaurus',
-        //       },
-        //     ],
-        //   },
-        // ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        logo: {
+          alt: 'MakinaRocks',
+          src: '/img/makinarocks.png',
+          href: 'https://makinarocks.ai',
+        },
+        copyright: `Copyright © 2021-${new Date().getFullYear()} MakinaRocks. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
